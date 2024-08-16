5ea6d972b7e31689e03ce1e50ee10c5ac6cf1192
KLZX121 <kenneth121lzx@gmail.com>
2024-08-16 22:08:23 +1000

    updated ui

--- main.js
+++ main.js
@@ -8,14 +8,16 @@ chrome.tabs.query(
         if (hostname == 'www.youtube.com') {
             heading.textContent = '';
             heading.style.display = 'none';
+
             // get vid id
             let searchParams = new URLSearchParams(url);
             let vidId = Array.from(searchParams.values())[0];
 
+            // get thumbnail
             let thumbnailUrl = `http://img.youtube.com/vi/${vidId}/maxresdefault.jpg`;
             imageDisp.src = thumbnailUrl;
 
-            document.body.style = 'width: 100%; height: 99%';
+            document.body.style = 'width: 100%; height: 100%';
         } else {
             heading.textContent = 'Not YouTube';
             imageDisp.style.display = 'none';
