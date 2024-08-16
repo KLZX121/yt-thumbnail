184f063a083e800d57bfb484afaaf05446105e5e
KLZX121 <kenneth121lzx@gmail.com>
2024-08-16 18:47:06 +1000

    added basic functionality

---
+++ main.js
@@ -0,0 +1,24 @@
+chrome.tabs.query(
+    {active: true, currentWindow: true},
+    tabs => {
+        // get url
+        let url = tabs[0].url;
+        let hostname = new URL(url).hostname;
+
+        if (hostname == 'www.youtube.com') {
+            heading.textContent = '';
+            heading.style.display = 'none';
+            // get vid id
+            let searchParams = new URLSearchParams(url);
+            let vidId = Array.from(searchParams.values())[0];
+
+            let thumbnailUrl = `http://img.youtube.com/vi/${vidId}/maxresdefault.jpg`;
+            imageDisp.src = thumbnailUrl;
+
+            document.body.style = 'width: 100%; height: 99%';
+        } else {
+            heading.textContent = 'Not YouTube';
+            imageDisp.style.display = 'none';
+        }
+    }
+)
